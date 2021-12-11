e40f8a1fe79aac8a20bbf4a4dd436a2706569ec8
Memo Chou <memochou1993@gmail.com>
2021-12-11 21:55:59 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const { WebSocketServer } = require('ws');
+
+const wss = new WebSocketServer({
+  port: 8080,
+});
+
+const origins = new Map();
+
+wss.on('connection', (ws, req) => {
+  const { origin } = req.headers;
+
+  origins.set(origin, origins.get(origin) + 1 || 1);
+
+  const timer = setInterval(() => {
+    ws.send(JSON.stringify({
+      count: origins.get(origin),
+    }));
+    console.log(origins);
+  }, 1000);
+
+  ws.on('message', () => {
+    // do nothing
+  });
+
+  ws.on('close', () => {
+    clearInterval(timer);
+    origins.get(origin) > 1
+      ? origins.set(origin, origins.get(origin) - 1)
+      : origins.delete(origin);
+  });
+});
